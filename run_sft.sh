1f9ea4b7d86213e501327839b2363644436c01c2
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-05-29 15:05:28 -0700

    fix run

--- run_sft.sh
+++ run_sft.sh
@@ -11,6 +11,7 @@ torchrun --nnodes=1  --nproc_per_node=8 -m verl.trainer.fsdp_sft_trainer \
         data.response_key=answer \
 	data.train_batch_size=32 \
         data.micro_batch_size_per_gpu=4 \
+	data.truncation=right \
 	model.partial_pretrain=$MODEL_NAME \
         trainer.project_name=system-sft \
 	trainer.experiment_name=system-7B \
