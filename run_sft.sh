a48844d4958ad5fb1fa34612f466b3bab84101b9
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-05-29 12:51:31 -0700

    addrunscript

---
+++ run_sft.sh
@@ -0,0 +1,18 @@
+export PYTHONPATH=$(pwd):${PYTHONPATH:-}
+train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_train_data.parquet
+test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_test_data.parquet
+ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data
+torchrun --nnodes=1  --nproc_per_node=8 -m verl.trainer.fsdp_sft_trainer \
+	data.train_files=$train_file \
+        data.val_files=$test_file \
+	data.prompt_key=prompt \
+        data.response_key=answer \
+	data.train_batch_size=4 \
+        data.micro_batch_size_per_gpu=4 \
+	model.partial_pretrain=$MODEL_NAME \
+        trainer.project_name=system-sft \
+	trainer.experiment_name=system-7B \
+        rainer.total_epochs=50 \
+	trainer.default_local_dir=$ckpt_dir \
+        trainer.logger=['console'] \
+	trainer.default_hdfs_dir=null
