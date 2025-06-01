771455b56907782b8e938a7e9f55cedc8b7156c5
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-05-31 19:16:14 -0700

    fixdisttrain

--- run_multinode_test.sh
+++ run_multinode_test.sh
@@ -1,5 +1,4 @@
-torchrun \
-  --nnodes=2 \
-  --nproc_per_node=8 \
-  --node_rank=$PAI_CURRENT_TASK_ROLE_CURRENT_TASK_INDEX \
-  multinode_ddp_test.py
+export NODE_RANK=$PAI_CURRENT_TASK_ROLE_CURRENT_TASK_INDEX
+export MASTER_ADDR=$PAI_TASK_ROLE_worker_TASK_0_HOST_IP
+export MASTER_PORT=29500
+torchrun --nnodes=2 --nproc_per_node=8 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT multinode_ddp_test.py
