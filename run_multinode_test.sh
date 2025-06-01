ad73799b2011425890fb8ba0465e083cba7409a0
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-05-31 21:05:56 -0700

    addport

--- run_multinode_test.sh
+++ run_multinode_test.sh
@@ -1,4 +1,4 @@
 export NODE_RANK=$PAI_CURRENT_TASK_ROLE_CURRENT_TASK_INDEX
 export MASTER_ADDR=$PAI_TASK_ROLE_worker_TASK_0_HOST_IP
 export MASTER_PORT=29500
-torchrun --nnodes=2 --nproc_per_node=8 --node_rank=${NODE_RANK} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} multinode_ddp_test.py
+torchrun --nnodes=2 --nproc_per_node=8 --node_rank=${NODE_RANK} --master_addr=${MASTER_ADDR} --master_port=25900 multinode_ddp_test.py
