156fa4a95ab71274b804b2f05f108eaaa06c42b2
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-05 12:04:40 -0700

    fixbatchsize

--- run_sft.sh
+++ run_sft.sh
@@ -28,8 +28,8 @@ python \
     data.val_files=$test_file \
     data.prompt_key=prompt \
     data.response_key=answer \
-    data.train_batch_size=128 \
-    data.micro_batch_size_per_gpu=8 \
+    data.train_batch_size=32 \
+    data.micro_batch_size_per_gpu=2 \
     data.max_length=4000 \
     trainer.project_name=system-sft \
     trainer.experiment_name=system-sft \
