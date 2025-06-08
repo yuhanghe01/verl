4e1e64b361976ecf964898556923c30c06577c19
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-08 10:42:01 -0700

    increasebatchsize

--- run_sft.sh
+++ run_sft.sh
@@ -11,8 +11,8 @@ python \
     data.val_files=$test_file \
     data.prompt_key=prompt \
     data.response_key=answer \
-    data.train_batch_size=64 \
-    data.micro_batch_size_per_gpu=4 \
+    data.train_batch_size=96 \
+    data.micro_batch_size_per_gpu=6 \
     data.max_length=4000 \
     trainer.project_name=system-sft \
     trainer.experiment_name=system-sft \
