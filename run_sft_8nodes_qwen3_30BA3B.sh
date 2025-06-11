f465ad2f303358d3f585b4b810ef3b6d64b3c0d5
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-10 23:15:48 -0700

    add30ba3b

--- run_sft_8nodes_qwen3_30BA3B.sh
+++ run_sft_8nodes_qwen3_30BA3B.sh
@@ -1,22 +1,26 @@
-MODEL_NAME=Qwen/Qwen3-30B-A3B
-export HF_HOME=/mnt/blob-data-sigmasystem/yuhang
+MODEL_NAME=Qwen/Qwen3-30B-A3B 
+python -c "import transformers; transformers.pipeline('text-generation', model='Qwen/Qwen2.5-Coder-32B')"
+
 train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_train_data.parquet
 test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_test_data.parquet
-ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data/ckpt_4nodes_system_qwen3_30BA3B
+ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data_Qwen3-30B-A3B
 
-torchrun --nnodes=8  --nproc_per_node=8 -m verl.trainer.fsdp_sft_trainer \
-	data.train_files=$train_file \
-        data.val_files=$test_file \
-	data.prompt_key=prompt \
-        data.response_key=answer \
-	data.train_batch_size=128 \
-        data.micro_batch_size_per_gpu=2 \
-	data.truncation=right \
-	data.max_length=4096 \
-	model.partial_pretrain=$MODEL_NAME \
-        trainer.project_name=system-sft \
-	trainer.experiment_name=system-qwen3_32B \
-        trainer.total_epochs=50 \
-	trainer.default_local_dir=$ckpt_dir \
-        trainer.logger=['console'] \
-	trainer.default_hdfs_dir=null
+python \
+    -m verl.trainer.main_sft \
+    data.train_files=$train_file \
+    data.val_files=$test_file \
+    data.prompt_key=prompt \
+    data.response_key=answer \
+    data.train_batch_size=2 \
+    data.max_length=4096 \
+    optim.lr=1e-5 \
+    model.model_path=$MODEL_NAME \
+    model.lora_rank=32 \
+    trainer.project_name=system-sft-qwen3-30b-a3b \
+    trainer.experiment_name=system-sft-qwen3-30b-a3b \
+    trainer.total_epochs=10 \
+    trainer.nnodes=8 \
+    trainer.n_gpus_per_node=8 \
+    trainer.logger=['wandb','console'] \
+    trainer.ckpt_save_dir=$ckpt_dir \
+    trainer.default_hdfs_dir=null
