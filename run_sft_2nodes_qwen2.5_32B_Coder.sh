0cc40373564c89a66de74257ea6851b1e08b783b
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-13 21:39:47 -0700

    add2nodes32BCode

---
+++ run_sft_2nodes_qwen2.5_32B_Coder.sh
@@ -0,0 +1,26 @@
+MODEL_NAME=Qwen/Qwen2.5-Coder-32B 
+python -c "import transformers; transformers.pipeline('text-generation', model='Qwen/Qwen2.5-Coder-32B')"
+
+train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_train_data.parquet
+test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/system_test_data.parquet
+ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data_Qwen2.5-Coder-32B
+
+python \
+    -m verl.trainer.main_sft \
+    data.train_files=$train_file \
+    data.val_files=$test_file \
+    data.prompt_key=prompt \
+    data.response_key=answer \
+    data.train_batch_size=2 \
+    data.max_length=4096 \
+    optim.lr=1e-5 \
+    model.model_path=$MODEL_NAME \
+    model.lora_rank=32 \
+    trainer.project_name=system-sft-qwen2.5-coder-32b \
+    trainer.experiment_name=system-sft-qwen2.5-coder-32b \
+    trainer.total_epochs=5 \
+    trainer.nnodes=2 \
+    trainer.n_gpus_per_node=8 \
+    trainer.logger=['wandb','console'] \
+    trainer.ckpt_save_dir=$ckpt_dir \
+    trainer.default_hdfs_dir=null
