7ced331c5da49fb001f5a830af2527f6639a9e11
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-21 17:47:45 -0700

    addprint

--- verl/utils/megatron_utils.py
+++ verl/utils/megatron_utils.py
@@ -674,6 +674,9 @@ def default_tp_concat_fn(layer_name_mapping, name, train_params, infer_params, m
     """
     from megatron.core import mpu
 
+    print('******************')
+    print(layer_name_mapping)
+    print('******************')
     if layer_name_mapping.get("qkv_layer_name") in name and "layer_norm" not in name:
         # if the tensor is qkv, for each param on tp, split into q, k, v
         # concat q, k, v separately.
