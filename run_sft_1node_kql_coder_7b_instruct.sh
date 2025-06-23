b544eec16b6a4dcb027c50aeb9db94634b2b8e02
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-23 16:00:50 -0700

    add8ktrain

--- run_sft_1node_kql_coder_7b_instruct.sh
+++ run_sft_1node_kql_coder_7b_instruct.sh
@@ -3,7 +3,7 @@ python -c "import transformers; transformers.pipeline('text-generation', model='
 
 train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/kql_parquet_0619/kql_train_data.parquet
 test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/kql_parquet_0619/kql_test_data.parquet
-ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data/Qwen2.5-7B-Coder-Instruct-Naive-kql-8k
+ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data/Qwen2.5-7B-Coder-Instruct-Naive-kql-8k-0623
 
 python \
     -m verl.trainer.main_sft_torchtrainer \
@@ -11,13 +11,13 @@ python \
     data.val_files=$test_file \
     data.prompt_key=prompt \
     data.response_key=answer \
-    data.train_batch_size=1 \
+    data.train_batch_size=2 \
     data.max_length=8000 \
-    data.truncation=left \
+    data.truncation=right \
     optim.lr=1e-5 \
     model.model_path=$MODEL_NAME \
-    trainer.project_name=kql-coder-7b-instruct-0620 \
-    trainer.experiment_name=kql-coder-7b-instruct-0620 \
+    trainer.project_name=kql-coder-7b-instruct-0623 \
+    trainer.experiment_name=kql-coder-7b-instruct-0623 \
     trainer.total_epochs=5 \
     trainer.nnodes=1 \
     trainer.n_gpus_per_node=8 \
