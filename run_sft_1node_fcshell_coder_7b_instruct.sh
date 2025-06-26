d5e08dc06e59b31173add1d7fe68aa743c0e29e6
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-06-26 12:56:35 -0700

    fcshellready

--- run_sft_1node_fcshell_coder_7b_instruct.sh
+++ run_sft_1node_fcshell_coder_7b_instruct.sh
@@ -1,9 +1,9 @@
 MODEL_NAME=Qwen/Qwen2.5-Coder-7B-Instruct 
 python -c "import transformers; transformers.pipeline('text-generation', model='Qwen/Qwen2.5-Coder-7B-Instruct')"
 
-train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/fcshell_parquet_0624/fcshell_train_data.parquet
-test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/fcshell_parquet_0624/fcshell_test_data.parquet
-ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data/Qwen2.5-7B-Coder-Instruct-Naive-fcshell-0624-8k
+train_file=/mnt/blob-data-sigmasystem/yuhang/system_data/fcshell_parquet_0626/fcshell_train_data.parquet
+test_file=/mnt/blob-data-sigmasystem/yuhang/system_data/fcshell_parquet_0626/fcshell_test_data.parquet
+ckpt_dir=/mnt/blob-data-sigmasystem-out/yuhang/system_data/Qwen2.5-7B-Coder-Instruct-Naive-fcshell-0626-8k
 
 python \
     -m verl.trainer.main_sft_torchtrainer \
@@ -11,13 +11,13 @@ python \
     data.val_files=$test_file \
     data.prompt_key=prompt \
     data.response_key=answer \
-    data.train_batch_size=1 \
-    data.max_length=8000 \
+    data.train_batch_size=3 \
+    data.max_length=4096 \
     data.truncation=left \
     optim.lr=1e-5 \
     model.model_path=$MODEL_NAME \
-    trainer.project_name=fcshell-coder-7b-instruct-0624 \
-    trainer.experiment_name=fcshell-coder-7b-instruct-0624 \
+    trainer.project_name=fcshell-coder-7b-instruct-0626 \
+    trainer.experiment_name=fcshell-coder-7b-instruct-0626 \
     trainer.total_epochs=5 \
     trainer.nnodes=1 \
     trainer.n_gpus_per_node=8 \
