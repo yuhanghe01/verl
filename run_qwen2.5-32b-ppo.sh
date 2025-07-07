6544671f687179b9d58ef751e776123f582c1cff
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-07-07 16:13:56 -0700

    removepadding

--- run_qwen2.5-32b-ppo.sh
+++ run_qwen2.5-32b-ppo.sh
@@ -27,8 +27,6 @@ python -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-0.5B-Instruct \
     actor_rollout_ref.model.enable_gradient_checkpointing=False \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.use_torch_compile=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
