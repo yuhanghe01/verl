26abbe2ff56d181ea22f8f1cc81bf273d4abe4e1
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-07-07 16:03:50 -0700

    fix32b

--- run_qwen2.5-32b-ppo.sh
+++ run_qwen2.5-32b-ppo.sh
@@ -36,7 +36,7 @@ python -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     critic.optim.lr=1e-5 \
