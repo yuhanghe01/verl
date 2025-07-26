45503fd9ffd3666ad8474047fcbe9f972bbb67c8
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-07-25 19:03:54 -0700

    reducebz

--- run_sft_1node_kql_coder_7b_instruct.sh
+++ run_sft_1node_kql_coder_7b_instruct.sh
@@ -11,8 +11,8 @@ python \
     data.val_files=$test_file \
     data.prompt_key=prompt \
     data.response_key=answer \
-    data.train_batch_size=2 \
-    data.max_length=8192 \
+    data.train_batch_size=1 \
+    data.max_length=8000 \
     data.truncation=right \
     optim.lr=1e-5 \
     model.model_path=$MODEL_NAME \
