6fe29f359a1457196bb0ef24194c345e95ea1473
Yuhang Henry <yuhanghenry@Yuhangs-MacBook-Air.local>
2025-09-13 23:05:12 -0700

    fixprocess

--- tango_flux_300h/tangoflux_train_300h.bash
+++ tango_flux_300h/tangoflux_train_300h.bash
@@ -2,7 +2,7 @@
 # Train TangoFlux with accelerate on GPUs 0 and 1
 
 # Select GPUs
-#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # Launch training FT
 accelerate launch \
